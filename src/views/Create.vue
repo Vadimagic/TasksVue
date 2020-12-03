96f74bc19c22299eee54fa33f0d4ef690fa48da9
Vadim <vadim.prog.2000@gmail.com>
2020-12-03 23:54:01 +0300

    create inputs for add task

--- src/views/Create.vue
+++ src/views/Create.vue
@@ -1,6 +1,19 @@
 <template>
 	<div>
-		<h1>Create</h1>
+		<div class="row">
+			<div class="col s6 offset-s3">
+				<h1 class="bold">Создать задачу</h1>
+				<div class="input-field">
+					<input id="title" type="text" class="validate" required>
+					<label for="title">Title</label>
+				</div>
+				<div class="chips" ref="chips"></div>
+				<div class="input-field">
+					<textarea id="textarea1" class="materialize-textarea"></textarea>
+					<label for="textarea1">Textarea</label>
+				</div>
+			</div>
+		</div>
 	</div>
 </template>
 
@@ -8,8 +21,10 @@
 
 export default {
 	name: 'Home',
-	components: {
-
+	mounted() {
+		M.Chips.init(this.$refs.chips, {
+			placeholder: 'Теги заданий'
+		});
 	}
 }
 </script>
