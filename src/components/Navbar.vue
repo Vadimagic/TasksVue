874181a4a2c110c12c37bb46f3fa2c55b1f19181
Vadim <vadim.prog.2000@gmail.com>
2020-12-03 23:37:15 +0300

    complete router vue

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,15 +1,17 @@
 <template>
-	<nav>
-		<div class="nav-wrapper">
-			<router-link href="#!" to="/" class="brand-logo">Задания</router-link>
-			<ul class="right hide-on-med-and-down">
-				<router-link tag="li" to="/" exact active-class="active">
-					<a href="sass.html">Добавить</a>
-				</router-link>
-				<router-link tag="li" to="/" active-class="active">
-					<a href="sass.html">Список</a>
-				</router-link>
-			</ul>
+	<nav class="deep-purple accent-2">
+		<div class="container">
+			<div class="nav-wrapper">
+				<router-link href="#!" to="/" class="brand-logo">Задания</router-link>
+				<ul class="right hide-on-med-and-down">
+					<router-link tag="li" to="/" exact active-class="active">
+						<a href="sass.html">Добавить</a>
+					</router-link>
+					<router-link tag="li" to="/list" active-class="active">
+						<a href="sass.html">Список</a>
+					</router-link>
+				</ul>
+			</div>
 		</div>
 	</nav>
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,25 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Create from '../views/Create.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
 	{
 		path: '/',
-		name: 'Home',
-		component: Home
-	}
+		name: 'create',
+		component: Create
+	},
+	{
+		path: '/list',
+		name: 'list',
+		component: () => import('../views/List.vue')
+	},
+	{
+		path: '/task/:id',
+		name: 'task',
+		component: () => import('../views/Task.vue')
+	},
 ]
 
 const router = new VueRouter({
