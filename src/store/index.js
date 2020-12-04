528dbfc0694927787dbb0048e293f9dd44144ec7
Vadim <vadim.prog.2000@gmail.com>
2020-12-04 04:49:23 +0300

    create add task to localStorage

--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,18 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
 	state: {
+		tasks: JSON.parse(localStorage.getItem('tasks') || '[]')
 	},
 	mutations: {
+		addTask(state, task) {
+			localStorage.setItem('tasks', JSON.stringify(task))
+			state.tasks = JSON.parse(localStorage.getItem('tasks') || '[]')
+		}
 	},
 	actions: {
+		addTask({commit}, task) {
+			commit('addTask', task)
+		}
 	},
 	modules: {
 	}
--- src/views/Create.vue
+++ src/views/Create.vue
@@ -3,16 +3,34 @@
 		<div class="row">
 			<div class="col s6 offset-s3">
 				<h1 class="bold">Создать задачу</h1>
-				<div class="input-field">
-					<input id="title" type="text" class="validate" required>
-					<label for="title">Title</label>
-				</div>
-				<div class="chips" ref="chips"></div>
-				<div class="input-field">
-					<textarea id="description" class="materialize-textarea" v-model="text"></textarea>
-					<label for="description">Textarea</label>
-					<span class="character-counter" style="float: right; font-size: 12px;">{{checkLengthText()}}/{{lengthText}}</span>
-				</div>
+
+				<form @submit.prevent="addTask">
+					<div class="input-field">
+						<input 
+							id="title" 
+							type="text" 
+							class="validate" 
+							required
+							v-model="title"
+						>
+						<label for="title">Title</label>
+					</div>
+
+					<div class="chips" ref="chips"></div>
+
+					<div class="input-field">
+						<textarea v-model="text" id="description" class="materialize-textarea"></textarea>
+						<label for="description">Textarea</label>
+						<span class="character-counter">{{checkLengthText()}}/{{lengthText}}</span>
+					</div>
+
+					<input type="text" class="datepicker" ref="datepicker">
+
+					<button class="btn waves-effect waves-light deep-purple" type="submit" name="action">
+						Создать задание
+						<i class="material-icons right">send</i>
+					</button>
+				</form>
 			</div>
 		</div>
 	</div>
@@ -25,7 +43,9 @@ export default {
 	data() {
 		return {
 			title: '',
+			tags: null,
 			text: '',
+			date: null,
 			lengthText: 1024
 		}
 	},
@@ -35,12 +55,44 @@ export default {
 				this.text = this.text.slice(0, this.lengthText)
 			}
 			return this.text.length
+		},
+		addTask() {
+			const task = {
+				id: Date.now(),
+				title: this.title,
+				tags: this.tags.chipsData,
+				text: this.text,
+				date: this.date.date,
+				status: 'в работе'
+			}
+
+			this.$store.dispatch('addTask', task);
 		}
 	},
 	mounted() {
-		M.Chips.init(this.$refs.chips, {
+		this.tags = M.Chips.init(this.$refs.chips, {
 			placeholder: 'Теги заданий'
 		});
+		this.date = M.Datepicker.init(this.$refs.datepicker, {
+			format: 'dd-mm-yyyy',
+			defaultDate: new Date(),
+			setDefaultDate: true
+		});
+	},
+	destroyed() {
+		if (this.date && this.date.destroy) {
+			this.date.destroy()
+		}
+		if (this.tags && this.tags.destroy) {
+			this.tags.destroy()
+		}
 	}
 }
 </script>
+
+<style lang="scss" scoped>
+	.character-counter {
+		float: right;
+		font-size: 12px;
+	}
+</style>
